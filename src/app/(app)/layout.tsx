029b9248a6119f48ec4b2ec7d4895d9dba62b709
Punarv Dinakar <tranquilblue91@gmail.com>
2025-06-25 18:43:36 +0530

    added investment assets to sidebar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,14 @@
 // app/(dashboard)/layout.tsx
 "use client";
 
-import { Home, LayoutDashboard, Menu, PieChart, Wallet } from "lucide-react";
+import {
+  BarChart3,
+  Home,
+  LayoutDashboard,
+  Menu,
+  PieChart,
+  Wallet,
+} from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -24,6 +31,11 @@ const sidebarItems = [
     title: "Spend Categories",
     href: "/spend-categories",
     icon: <Wallet className="h-5 w-5" />,
+  },
+  {
+    title: "Investment Assets",
+    href: "/investment-assets",
+    icon: <BarChart3 className="h-5 w-5" />,
   },
 ];
 
